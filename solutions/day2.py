4ce47221d951345704c3050a1ea6daf46b4fd3c1
Maximus Shurr <maxshurr@gmail.com>
2025-12-02 13:44:19 -0500

    solve day 2 part 1

---
+++ solutions/day2.py
@@ -0,0 +1,27 @@
+from math import log10
+
+with open('input/day2.txt') as f:
+    lines = f.read().strip()
+
+def createInvalidId(repeatedDigit):
+    if repeatedDigit < 1:
+        return 11
+    return repeatedDigit + (repeatedDigit * 10**int(log10(repeatedDigit)+1))
+
+def part1(rangeString = lines):
+    ranges = rangeString.split(',')
+    invalidIds = set()
+    for r in ranges:
+        start, end = map(int, r.split('-'))
+        startRepeatedDigit = start // 10**int(log10(start) // 2 + 1)
+        currentInvalidId = createInvalidId(startRepeatedDigit)
+        while currentInvalidId <= end:
+            if currentInvalidId >= start:
+                invalidIds.add(currentInvalidId)
+            startRepeatedDigit += 1
+            currentInvalidId = createInvalidId(startRepeatedDigit)
+    return sum(invalidIds)
+
+# exampleInput = "11-22,95-115,998-1012,1188511880-1188511890,222220-222224,1698522-1698528,446443-446449,38593856-38593862,565653-565659,824824821-824824827,2121212118-2121212124"
+# print(part1(exampleInput))
+print(part1())
