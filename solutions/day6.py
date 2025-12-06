4c4d9d3130d19cd921ae689822a428ae5c649607
Maximus Shurr <maxshurr@gmail.com>
2025-12-06 08:51:59 -0500

    solve day 6 part 2

--- solutions/day6.py
+++ solutions/day6.py
@@ -19,11 +19,40 @@ def part1(data = input):
             total_sum += sum([parsed_numbers[j][i] for j in range(len(parsed_numbers))])
     return total_sum
 
-example = [
-    "123 328  51 64 ",
-    " 45 64  387 23 ",
-    "  6 98  215 314",
-    "*   +   *   +  ",
-]
-print(part1(example))
+def part2(data = input):
+    colIndex = len(data[0]) - 1
+    totalSum = 0
+    currentColNumbers = []
+    while colIndex >= 0:
+        colNumber = 0
+        rowIndex = 0
+        while rowIndex < len(data) - 1:
+            try:
+                cellNumber = int(data[rowIndex][colIndex])
+                colNumber = colNumber * 10 + cellNumber
+            except ValueError:
+                pass
+            rowIndex += 1
+        currentColNumbers.append(colNumber)
+        if data[rowIndex][colIndex] == '*':
+            totalSum += reduce(lambda x, y: x * y, currentColNumbers)
+            currentColNumbers = []
+            colIndex -= 2
+        elif data[rowIndex][colIndex] == '+':
+            totalSum += sum(currentColNumbers)
+            currentColNumbers = []
+            colIndex -= 2
+        else:
+            colIndex -= 1
+    return totalSum
+
+# example = [
+#     "123 328  51 64 ",
+#     " 45 64  387 23 ",
+#     "  6 98  215 314",
+#     "*   +   *   +  ",
+# ]
+# print(part1(example))
+# print(part2(example))
 print(part1())
+print(part2())
