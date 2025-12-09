d7a6da7b26a9f96b553349448b23113e1f3f35a1
Maximus Shurr <maxshurr@gmail.com>
2025-12-09 10:41:30 -0500

    solve day 9 part 2
    - I attempted my own solution, but experienced the difficult of boundary conditions and range manipulations
    - found well supported shapely to do the heavy-lifting

--- solutions/day9.py
+++ solutions/day9.py
@@ -1,5 +1,6 @@
-import heapq
-from collections import Counter
+from shapely.geometry import Polygon, box
+from shapely.prepared import prep
+from itertools import combinations
 
 with open('input/day9.txt') as f:
     input = f.read().splitlines()
@@ -12,22 +13,42 @@ def part1(data = input):
     n = len(allPoints)
 
     maxArea = 0
-    maxPoints = (None, None)
     for i in range(n):
         for j in range(i + 1, n):
             maxArea = max(maxArea, areaBetweenPoints(allPoints[i], allPoints[j]))
 
     return maxArea
 
-example = [
-    "7,1",
-    "11,1",
-    "11,7",
-    "9,7",
-    "9,5",
-    "2,5",
-    "2,3",
-    "7,3",
-]
-print(part1(example))
+def part2(data=input):
+    pts = getPoints(data)
+    poly = Polygon(pts)
+    poly_prep = prep(poly)   # speed for O(n^2) tests
+
+    max_area = 0
+
+    for (x1,y1),(x2,y2) in combinations(pts,2):
+        xmin,xmax = sorted([x1,x2])
+        ymin,ymax = sorted([y1,y2])
+
+        rect = box(xmin,ymin,xmax,ymax)
+
+        if poly_prep.covers(rect):  # inclusive boundary test
+            area = (xmax-xmin+1)*(ymax-ymin+1)
+            max_area = max(max_area, area)
+
+    return max_area
+
+# example = [
+#     "7,1",
+#     "11,1",
+#     "11,7",
+#     "9,7",
+#     "9,5",
+#     "2,5",
+#     "2,3",
+#     "7,3",
+# ]
+# print(part1(example))
+# print(part2(example))
 print(part1())
+print(part2())
